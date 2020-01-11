8e64ab0952e2066cb9c53dc9c300bd08ba194fb5
nemixe <rafel.ami@gmail.com>
2020-01-11 21:42:02 +0700

    slicing done

---
+++ components/buttons/Base.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input
+      class="text-lg py-3 rounded-full my-1 shadow"
+      :class="{ 'w-full': isfluid, ...typeClasses }"
+      type="button"
+      :value="text"
+      @click="click"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    isfluid: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    click: {
+      type: Function,
+      default: function() {}
+    }
+  },
+  data: function() {
+    return {
+      typeClasses: {
+        'bg-primary text-white': this.type === 'primary',
+        'bg-white text-gray-500': this.type === 'secondary',
+      }
+    }
+  }
+}
+</script>
